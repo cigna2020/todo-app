7121c227249d27090d67b54123f79dd166eb7bd5
Alex <cigan@ua.fm>
2021-04-23 12:09:52 +0300

    US002: fix, change the method innerHTML into insertAdjacentHTML

--- js/modules/createItem.js
+++ js/modules/createItem.js
@@ -11,16 +11,15 @@ export class Item {
   }
 
   createListElement(value) {
-    let newTaskItem = document.createElement('li');
-    this.LIST_TASK_IN_PROGRESS.append(newTaskItem);
-    newTaskItem.classList.add('progress-item');
-    newTaskItem.innerHTML = `
+    this.LIST_TASK_IN_PROGRESS.insertAdjacentHTML('beforeend', `
+          <li class="progress-item">
           <label class="label-progress">${value}</label>
           <input type="text" class="progress-input" placeholder="${value}">
           <button class="progress-btn" id="done-task" title="mark as done">&#10004;</button>
           <button class="progress-btn" id="delete-task" title="delete task">&#10008;</button>
           <button class="progress-btn" id="edit-task" title="edit your task">&#10002;</button>
-          `
+          </li>
+          `)
   }
 
   checkLengthOfInputValue(value) {
