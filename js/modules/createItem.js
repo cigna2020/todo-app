6a33e692debe8bc9191b157af6d70e3a0bd46f1b
Alex <cigan@ua.fm>
2021-04-23 14:21:25 +0300

    US002: fix, divide the method to create and render element on two separate methods

--- js/modules/createItem.js
+++ js/modules/createItem.js
@@ -11,16 +11,21 @@ export class Item {
     this.setHandler();
   }
 
-  createListElement(value) {
-    this.listTaskInProgress.insertAdjacentHTML('beforeend', `
-          <li class="progress-item">
+  createListItem(value) {
+    const newListItem = document.createElement('li');
+    newListItem.classList.add('progress-item');
+    newListItem.insertAdjacentHTML('beforeend', `
           <label class="label-progress">${value}</label>
           <input type="text" class="progress-input" placeholder="${value}">
           <button class="progress-btn" id="done-task" title="mark as done">&#10004;</button>
           <button class="progress-btn" id="delete-task" title="delete task">&#10008;</button>
           <button class="progress-btn" id="edit-task" title="edit your task">&#10002;</button>
-          </li>
           `)
+    return newListItem;
+  }
+
+  renderListItem(parentElem, childElem) {
+    parentElem.append(childElem);
   }
 
   resetInputValue() {
@@ -32,7 +37,7 @@ export class Item {
       e.preventDefault();
       const inputValue = this.inputNewTask.value;
       Validator.checkLengthOfInputValue(inputValue);
-      this.createListElement(inputValue);
+      this.renderListItem(this.listTaskInProgress, this.createListItem(inputValue));
       this.resetInputValue();
     })
   }
